917d3bab5423800f60d1cf47e87974ac41807c5c
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-08-21 09:11:07 +0300

    feat: added saving to localstorage

---
+++ src/services/storage.ts
@@ -0,0 +1,23 @@
+import type {Task} from "../types/table.ts";
+
+const   STORAGE_KEY = "tasks"
+
+
+export const storage = {
+    getAllTasks: (): Task[] => {
+        const data = localStorage.getItem(STORAGE_KEY)
+        return data ? JSON.parse(data) : []
+    },
+
+
+    addTask(task: Task): void {
+        const tasks = storage.getAllTasks()
+        tasks.push(task)
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks))
+    },
+
+    removeTask(task: Task): void {
+        const tasks = storage.getAllTasks().filter(t => t.id !== task.id)
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks))
+    }
+}
